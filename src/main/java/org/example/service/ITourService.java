a3b174e4ea23a30a837a5fd0272df71cc1e49fde
levancuon <levancuongprovip@gmail.com>
2024-06-25 20:06:08 +0700

    anwdanwkjd

---
+++ src/main/java/org/example/service/ITourService.java
@@ -0,0 +1,19 @@
+package org.example.service;
+
+import org.example.model.Tour;
+import org.example.model.Type;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface ITourService extends IGenericService<Tour> {
+    Iterable<Tour> findAllByType(Type type);
+
+    Page<Tour> findAll(Pageable pageable);
+
+
+    Page<Tour> findByCodeEndingWith(Pageable pageable,String code);
+
+    Page<Tour> findAllAndSort(String sort);
+}
